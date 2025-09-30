c63a7ad853524ec387c4d95ebf9b4547f7b72d9e
Hrisikeshhh <hrisikeshgogoi09@gmail.com>
2025-09-30 17:05:07 +0530

    add

--- script.js
+++ script.js
@@ -427,6 +427,60 @@ discovery.forEach((item)=>{
 })
 
 
+                //  clients Counter
+
+ function animateCount(element, target) {
+    let count = 0;
+    let step = Math.ceil(target / 100); // speed of increase
+    let interval = setInterval(() => {
+      count += step;
+      if (count >= target) {
+        count = target; // stop at target
+        clearInterval(interval);
+      }
+      element.textContent = count;
+    }, 20); // update every 20ms
+  }
+
+
+
+  // Use IntersectionObserver so it starts when div is visible
+  const clientDiv = document.getElementById("ProjectDone");
+  const observer = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting) {
+      animateCount(clientDiv, 1); // suppose 1500 clients
+      observer.disconnect(); // run only once
+    }
+  });
+
+  observer.observe(clientDiv);
+
+
+
+  // Use IntersectionObserver so it starts when div is visible
+  const clientDiv1 = document.getElementById("happyClientServed");
+  const observer1 = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting) {
+      animateCount(clientDiv1, 1); // suppose 1500 clients
+      observer1.disconnect(); // run only once
+    }
+  });
+
+  observer1.observe(clientDiv1);
+
+
+
+  // Use IntersectionObserver so it starts when div is visible
+  const clientDiv2 = document.getElementById("creativeDesignDelivered");
+  const observer2 = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting) {
+      animateCount(clientDiv2, 1); // suppose 1500 clients
+      observer2.disconnect(); // run only once
+    }
+  });
+
+  observer2.observe(clientDiv2);
+
 
 
 
